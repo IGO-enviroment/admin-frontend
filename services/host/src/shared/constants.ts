41770cfdb2241487c12653b7036c0b23d4377dea
DAGribanov <DAGribanov@nota.tech>
2024-06-03 01:38:57 +0500

    url

--- services/host/src/shared/constants.ts
+++ services/host/src/shared/constants.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { getCookie } from "@/shared/cookies/get";
 
-export const baseURL = "https://98c1-178-46-71-134.ngrok-free.app/";
+// export const baseURL = "https://98c1-178-46-71-134.ngrok-free.app/";
+export const baseURL = "https://api.museum-ekb.ru/";
 
 const ApiInstance = axios.create({
    baseURL,
